626491b1b30c08e4ae2f1282bedd51d158ff122e
Lea-Vangheluwe <lv.vangheluwe@gmail.com>
2021-10-21 10:52:30 +0200

    creation composant listeFilm

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListeFilmComponent } from './liste-film/liste-film.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListeFilmComponent
   ],
   imports: [
     BrowserModule,
