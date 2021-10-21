e21a4de1dfd199d131c4e3ada41276d600159ff9
Lea-Vangheluwe <lv.vangheluwe@gmail.com>
2021-10-21 11:04:09 +0200

    ajout methode getMovie

--- src/app/shared/services/movies.service.ts
+++ src/app/shared/services/movies.service.ts
@@ -1,14 +1,22 @@
+import { HttpHeaders, HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { Movie } from '../interfaces/movie';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MoviesService {
 
-  constructor() { }
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type':  'application/json'
+    })
+  };
+  constructor(private httpClient: HttpClient) { }
 
   getMovie(){
-    
+    return this.httpClient.get<Movie[]>(`${environment.serverUrl}/movies`)
   };
 
   upScore(){
